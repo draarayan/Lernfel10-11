496a63c472fe593e44a62fd6be7c811c26775b7e
BenKunze <ben.kunze@edeka.de>
2024-09-04 09:19:52 +0200

    Login Seite

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,12 +5,22 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ItemListComponent } from './item-list/item-list.component';
+import { LoginComponent } from './login/login.component'; // Importiere die LoginComponent
 
-@NgModule({ declarations: [
+@NgModule({
+    declarations: [
         AppComponent,
-        ItemListComponent // Stelle sicher, dass dies hier enthalten ist
+        ItemListComponent,
+        LoginComponent // Füge die LoginComponent zur Deklarationsliste hinzu
     ],
-    bootstrap: [AppComponent], imports: [BrowserModule,
-        ReactiveFormsModule, // Importiert das ReactiveFormsModule
-        AppRoutingModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
+    bootstrap: [AppComponent],
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        AppRoutingModule
+    ],
+    providers: [
+        provideHttpClient(withInterceptorsFromDi())
+    ]
+})
 export class AppModule { }
---
+++ frontend/src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required]
+    });
+  }
+
+  onSubmit(): void {
+    if (this.loginForm.valid) {
+      const { email, password } = this.loginForm.value;
+      console.log('Login successful:', email, password);
+      // Füge hier die Logik zum Einloggen hinzu, z.B. API-Aufruf
+    }
+  }
+}
