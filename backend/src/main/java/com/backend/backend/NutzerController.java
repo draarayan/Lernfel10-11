003244d9679ea4aca043937e6bb6a9cb926f1b0a
BenKunze <ben.kunze@edeka.de>
2024-12-03 13:41:25 +0100

    Löschen anfrage-liste

--- backend/src/main/java/com/backend/backend/NutzerController.java
+++ backend/src/main/java/com/backend/backend/NutzerController.java
@@ -5,7 +5,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.backend.backend.dto.PasswordChangeRequest;
 import com.backend.backend.repository.EventRepository;
 
 import jakarta.servlet.http.HttpServletRequest;
